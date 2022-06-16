22c5caafd89dbb77ba966cf23ab8a31b42b19df1
aboongm <aboongm@yahoo.com>
2022-06-16 14:26:26 +0530

    Add gemfile and install rubocop, add code for filter method in my_enumerable, and corrects linting errors

---
+++ my_list.rb
@@ -0,0 +1,27 @@
+require_relative 'my_enumerable'
+
+class MyList
+  include MyEnumerable
+
+  def initialize(*list)
+    @list = list
+  end
+
+  def each(&)
+    @list.each(&)
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+# All method
+p (list.all? { |e| e < 5 })
+p (list.all? { |e| e > 5 })
+
+# Any method
+p (list.any? { |e| e == 2 })
+p (list.any? { |e| e == 5 })
+# true
+
+# filter method
+p list.filter(&:even?)
+#  [2, 4]
