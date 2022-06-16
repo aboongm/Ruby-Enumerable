3d5e9dcdd4b88f5d13bf63792c0c313854a71e5c
aboongm <aboongm@yahoo.com>
2022-06-16 14:31:22 +0530

    Correction for linting errors

--- my_list.rb
+++ my_list.rb
@@ -14,12 +14,12 @@ end
 
 list = MyList.new(1, 2, 3, 4)
 # All method
-p (list.all? { |e| e < 5 })
-p (list.all? { |e| e > 5 })
+p(list.all? { |e| e < 5 })
+p(list.all? { |e| e > 5 })
 
 # Any method
-p (list.any? { |e| e == 2 })
-p (list.any? { |e| e == 5 })
+p(list.any? { |e| e == 2 })
+p(list.any? { |e| e == 5 })
 # true
 
 # filter method
